24c43670538f98469c33fbcda0879332acd50a96
rob <rmillikin@wisc.edu>
2017-06-01 15:17:31 -0500

    edit assembly info

--- FlashLFQ/Properties/AssemblyInfo.cs
+++ FlashLFQ/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FlashLFQ")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Ultrafast Label-free Protein/Peptide Quantification")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Robert M.")]
 [assembly: AssemblyProduct("FlashLFQ")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
