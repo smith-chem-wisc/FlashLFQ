b2eb6656619359a6aa0ee1595ba0286b857d6b4b
trishorts <mshort@chem.wisc.edu>
2022-01-10 14:54:24 -0600

    comment dynamic header reading

--- Util/ScanInfoRecovery.cs
+++ Util/ScanInfoRecovery.cs
@@ -47,99 +47,99 @@ namespace Util
             }
             return shi;
         }
-        public static double RetentionTimeFromScanNumber(string fullFilePathWithExtension, int oneBasedScanNumber)
-        {
-            switch (GetDataFileType(fullFilePathWithExtension))
-            {
-                case DataFileType.Thermo:
-                    ThermoDynamicData thermoDynamicConnection = null;
-                    try
-                    {
-                        thermoDynamicConnection = new ThermoDynamicData(fullFilePathWithExtension);
-                        MsDataScan scan = thermoDynamicConnection.GetOneBasedScanFromDynamicConnection(oneBasedScanNumber, null);
-                        thermoDynamicConnection.CloseDynamicConnection();
-                        return scan.RetentionTime;
-                    }
-                    catch (FileNotFoundException)
-                    {
-                        if (thermoDynamicConnection != null)
-                        {
-                            thermoDynamicConnection.CloseDynamicConnection();
-                            Console.WriteLine("FlashLFQ Error: Can't find data file" + fullFilePathWithExtension + "\n");
-                        }
+        //public static double RetentionTimeFromScanNumber(string fullFilePathWithExtension, int oneBasedScanNumber)
+        //{
+        //    switch (GetDataFileType(fullFilePathWithExtension))
+        //    {
+        //        case DataFileType.Thermo:
+        //            ThermoDynamicData thermoDynamicConnection = null;
+        //            try
+        //            {
+        //                thermoDynamicConnection = new ThermoDynamicData(fullFilePathWithExtension);
+        //                MsDataScan scan = thermoDynamicConnection.GetOneBasedScanFromDynamicConnection(oneBasedScanNumber, null);
+        //                thermoDynamicConnection.CloseDynamicConnection();
+        //                return scan.RetentionTime;
+        //            }
+        //            catch (FileNotFoundException)
+        //            {
+        //                if (thermoDynamicConnection != null)
+        //                {
+        //                    thermoDynamicConnection.CloseDynamicConnection();
+        //                    Console.WriteLine("FlashLFQ Error: Can't find data file" + fullFilePathWithExtension + "\n");
+        //                }
 
-                        return Double.NaN;
-                    }
-                    catch (Exception e)
-                    {
-                        if (thermoDynamicConnection != null)
-                        {
-                            thermoDynamicConnection.CloseDynamicConnection();
-                            throw new MzLibException("FlashLFQ Error: Problem opening data file " + fullFilePathWithExtension + "; " + e.Message);
-                        }
-                        return Double.NaN;
-                    }
-                case DataFileType.mzML:
-                    MzmlDynamicData mzmlDynamicConnection = null;
-                    try
-                    {
-                        mzmlDynamicConnection = new MzmlDynamicData(fullFilePathWithExtension);
-                        MsDataScan scan = mzmlDynamicConnection.GetOneBasedScanFromDynamicConnection(oneBasedScanNumber, null);
-                        mzmlDynamicConnection.CloseDynamicConnection();
-                        return scan.RetentionTime;
-                    }
-                    catch (FileNotFoundException)
-                    {
-                        if (mzmlDynamicConnection != null)
-                        {
-                            mzmlDynamicConnection.CloseDynamicConnection();
-                            Console.WriteLine("FlashLFQ Error: Can't find data file" + fullFilePathWithExtension + "\n");
-                        }
+        //                return Double.NaN;
+        //            }
+        //            catch (Exception e)
+        //            {
+        //                if (thermoDynamicConnection != null)
+        //                {
+        //                    thermoDynamicConnection.CloseDynamicConnection();
+        //                    throw new MzLibException("FlashLFQ Error: Problem opening data file " + fullFilePathWithExtension + "; " + e.Message);
+        //                }
+        //                return Double.NaN;
+        //            }
+        //        case DataFileType.mzML:
+        //            MzmlDynamicData mzmlDynamicConnection = null;
+        //            try
+        //            {
+        //                mzmlDynamicConnection = new MzmlDynamicData(fullFilePathWithExtension);
+        //                MsDataScan scan = mzmlDynamicConnection.GetOneBasedScanFromDynamicConnection(oneBasedScanNumber, null);
+        //                mzmlDynamicConnection.CloseDynamicConnection();
+        //                return scan.RetentionTime;
+        //            }
+        //            catch (FileNotFoundException)
+        //            {
+        //                if (mzmlDynamicConnection != null)
+        //                {
+        //                    mzmlDynamicConnection.CloseDynamicConnection();
+        //                    Console.WriteLine("FlashLFQ Error: Can't find data file" + fullFilePathWithExtension + "\n");
+        //                }
 
-                        return Double.NaN;
-                    }
-                    catch (Exception e)
-                    {
-                        if (mzmlDynamicConnection != null)
-                        {
-                            mzmlDynamicConnection.CloseDynamicConnection();
-                            throw new MzLibException("FlashLFQ Error: Problem opening data file " + fullFilePathWithExtension + "; " + e.Message);
-                        }
-                        return Double.NaN;
-                    }
-                case DataFileType.mgf:
-                    MgfDynamicData mgfDynamicConnection = null;
-                    try
-                    {
-                        mgfDynamicConnection = new MgfDynamicData(fullFilePathWithExtension);
-                        MsDataScan scan = mgfDynamicConnection.GetOneBasedScanFromDynamicConnection(oneBasedScanNumber, null);
-                        mgfDynamicConnection.CloseDynamicConnection();
-                        return scan.RetentionTime;
-                    }
-                    catch (FileNotFoundException)
-                    {
-                        if (mgfDynamicConnection != null)
-                        {
-                            mgfDynamicConnection.CloseDynamicConnection();
-                            Console.WriteLine("FlashLFQ Error: Can't find data file" + fullFilePathWithExtension + "\n");
-                        }
+        //                return Double.NaN;
+        //            }
+        //            catch (Exception e)
+        //            {
+        //                if (mzmlDynamicConnection != null)
+        //                {
+        //                    mzmlDynamicConnection.CloseDynamicConnection();
+        //                    throw new MzLibException("FlashLFQ Error: Problem opening data file " + fullFilePathWithExtension + "; " + e.Message);
+        //                }
+        //                return Double.NaN;
+        //            }
+        //        case DataFileType.mgf:
+        //            MgfDynamicData mgfDynamicConnection = null;
+        //            try
+        //            {
+        //                mgfDynamicConnection = new MgfDynamicData(fullFilePathWithExtension);
+        //                MsDataScan scan = mgfDynamicConnection.GetOneBasedScanFromDynamicConnection(oneBasedScanNumber, null);
+        //                mgfDynamicConnection.CloseDynamicConnection();
+        //                return scan.RetentionTime;
+        //            }
+        //            catch (FileNotFoundException)
+        //            {
+        //                if (mgfDynamicConnection != null)
+        //                {
+        //                    mgfDynamicConnection.CloseDynamicConnection();
+        //                    Console.WriteLine("FlashLFQ Error: Can't find data file" + fullFilePathWithExtension + "\n");
+        //                }
 
-                        return Double.NaN;
-                    }
-                    catch (Exception e)
-                    {
-                        if (mgfDynamicConnection != null)
-                        {
-                            mgfDynamicConnection.CloseDynamicConnection();
-                            throw new MzLibException("FlashLFQ Error: Problem opening data file " + fullFilePathWithExtension + "; " + e.Message);
-                        }
-                        return Double.NaN;
-                    }
-                case DataFileType.unknown:
-                default:
-                    throw new MzLibException("FlashLFQ Error: Unknown data file extension (.raw, .mzML, .mgf expected): " + fullFilePathWithExtension);
-            }
-        }
+        //                return Double.NaN;
+        //            }
+        //            catch (Exception e)
+        //            {
+        //                if (mgfDynamicConnection != null)
+        //                {
+        //                    mgfDynamicConnection.CloseDynamicConnection();
+        //                    throw new MzLibException("FlashLFQ Error: Problem opening data file " + fullFilePathWithExtension + "; " + e.Message);
+        //                }
+        //                return Double.NaN;
+        //            }
+        //        case DataFileType.unknown:
+        //        default:
+        //            throw new MzLibException("FlashLFQ Error: Unknown data file extension (.raw, .mzML, .mgf expected): " + fullFilePathWithExtension);
+        //    }
+        //}
 
         private static DataFileType GetDataFileType(string fullFilePathWithExtension)
         {
