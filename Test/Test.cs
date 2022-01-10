556fd6d5e52f28e332a6df6cf70d156bb628af15
trishorts <mshort@chem.wisc.edu>
2022-01-10 10:55:06 -0600

    minimal percolator unit test with small calibratable yeast mzml

--- Test/Test.cs
+++ Test/Test.cs
@@ -161,6 +161,43 @@ namespace Test
         }
 
         [Test]
+        public static void TestPercolatorOutput()
+        {
+            string search = "Percolator";
+            string psmFilename = "PercolatorSmallCalibratableYeast.txt";
+
+            var myDirectory = Path.Combine(TestContext.CurrentContext.TestDirectory, "SampleFiles");
+            var pathOfIdentificationFile = Path.Combine(myDirectory, search, psmFilename);
+            var pathOfMzml = Path.Combine(myDirectory, "SmallCalibratible_Yeast.mzML");
+            Assert.That(File.Exists(pathOfIdentificationFile));
+            Assert.That(File.Exists(pathOfMzml));
+
+            string[] myargs = new string[]
+            {
+                "--rep",
+                myDirectory,
+                "--idt",
+                pathOfIdentificationFile,
+                "--ppm",
+                "5"
+            };
+
+            CMD.FlashLfqExecutable.Main(myargs);
+
+            string peaksPath = Path.Combine(myDirectory, search, "QuantifiedPeaks.tsv");
+            Assert.That(File.Exists(peaksPath));
+            File.Delete(peaksPath);
+
+            string peptidesPath = Path.Combine(myDirectory, search, "QuantifiedPeptides.tsv");
+            Assert.That(File.Exists(peptidesPath));
+            File.Delete(peptidesPath);
+
+            string proteinsPath = Path.Combine(myDirectory, search, "QuantifiedProteins.tsv");
+            Assert.That(File.Exists(proteinsPath));
+            File.Delete(proteinsPath);
+        }
+
+        [Test]
         public static void TestGenericOutput()
         {
             string search = "Generic";
